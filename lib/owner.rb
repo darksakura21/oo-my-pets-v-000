af970362b14880512496db3bf5c966b0d177e663
Ruth Mesfun <ruth@flatironschool.com>
2017-10-10 18:42:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,7 @@ class Owner
 
   def sell_pets
     self.pets.each_value {|pets| pets.clear}
-    self.pet.select {|type,pet| instance.mood = "nervous"};
+    self.pet.select {|type,pet| pet.mood = "nervous"};
     # self.pets[:cats].select{|cat| cat.mood = "nervous"};
     # self.pets[:fishes].select{|fish| fish.mood = "nervous"};
     # self.pets[:dogs].select{|dog| dog.mood = "nervous"};
